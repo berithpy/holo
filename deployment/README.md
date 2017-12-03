ae8cd49722e75551a568fd3f83e0417d6e4f8ee8
berith <berith.py@gmail.com>
2017-12-03 02:40:48 +0000

    Devops?idk.

---
+++ deployment/README.md
@@ -0,0 +1,4 @@
+This files goes here, this files goes there yadda yadda
+/etc/systemd/system/gunicorn.service gunicorn.service
+/etc/systemd/system/gunicorn.socket gunicorn.socket
+/etc/nginx/sites-available/holo.conf holo.conf
--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response,render_template,redirect
 from matrix import scrolling_text_flask
+from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
@@ -16,5 +17,6 @@ def api():
 		scrolling_text_flask(msg,speed,font)
 		return redirect('/',code=302)
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5123)
