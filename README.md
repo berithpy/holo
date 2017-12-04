250bd8156fba508f5be8d90e2eb63a23c7bf5e62
Diego Diaz <berith.py@gmail.com>
2017-12-03 21:14:34 -0300

    Fixes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # holo_microservice
+Tiny website to run on a raspberry pi (or equivalent) to write stuff on led matrixes!   
+Called holo after the led inventor Nick Holonyak!   
+
+The project relies heavily on the luma project to draw on the matrix!   
+
+TODO
+cleanup matrix.py
+improve website
+documentation
--- matrix.py
+++ matrix.py
@@ -2,8 +2,6 @@
 import re
 import time
 import argparse
-import keyboard
-
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -39,23 +37,31 @@ def ascii_char(device,pic):
 		text(draw, (0, 0), chr(pic), fill="white")
 	time.sleep(2)
 
-def keyboard_2(device):
-	if keyboard.is_pressed('up'):#if key 'q' is pressed 
-		ascii_char(device,24)
-		pass
-	elif keyboard.is_pressed('down'):
-		ascii_char(device,25)
-		pass
-	elif keyboard.is_pressed('left'):
-		ascii_char(device,27)
-		pass
-	elif keyboard.is_pressed('right'):
-		ascii_char(device,26)
-		pass
-	else:
-		pass
+
+# def keyboard_2(device):
+# 	if keyboard.is_pressed('up'):#if key 'q' is pressed 
+# 		ascii_char(device,24)
+# 		pass
+# 	elif keyboard.is_pressed('down'):
+# 		ascii_char(device,25)
+# 		pass
+# 	elif keyboard.is_pressed('left'):
+# 		ascii_char(device,27)
+# 		pass
+# 	elif keyboard.is_pressed('right'):
+# 		ascii_char(device,26)
+# 		pass
+# 	else:
+# 		pass
 		
 def scrolling_text_flask(msg,speed,font_index=1):
+	"""
+	This is the code that gets called by the flask app
+	msg: String message to scroll(Not UTF-8 compilant)
+	speed: speed delay. The higher, the slower.
+	font_index: it was easier for us to use numbers 
+	instead of the actual font names.
+	"""
 	serial = spi(port=0, device=0, gpio=noop())
 	device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
 	fonts=[CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
@@ -63,6 +69,10 @@ def scrolling_text_flask(msg,speed,font_index=1):
 			
 
 if __name__ == "__main__":
+	"""
+	Testing code!
+	You shouldn't worry about it unless you want to test your led matrix beforehand.
+	"""
 	serial = spi(port=0, device=0, gpio=noop())
 	device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
 	fonts=[CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
