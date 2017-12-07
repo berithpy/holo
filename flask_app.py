5223cb30089a591f7e5e4a5ee43a742801461678
Alejo Carballude <alejocarballude@gmail.com>
2017-12-07 04:35:09 -0300

    Enabled Flask CORS capabilities. Added test API endpoints. Translated parameters to English to improve API consistency.

--- flask_app.py
+++ flask_app.py
@@ -1,24 +1,39 @@
-from flask import Flask, request, Response,render_template,redirect, url_for
-from matrix import scrolling_text_flask
+from flask import Flask, request, Response, render_template, redirect, url_for
+from flask_cors import CORS
+from matrix import test_scrolling_text_flask, scrolling_text_flask
 from werkzeug.contrib.fixers import ProxyFix
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
-	return render_template('scrolling_text.html')
+    return render_template('scrolling_text.html')
 
-@app.route("/text/scrolling/", methods=['POST'])
-def api():
-	if hasattr(request,'form'):
-		msg=request.form['texto']
-		speed=(11-int(request.form.get('speed')))*0.01
-		font=int(request.form.get('font'))
-		print("speed {} font {}".format(speed,font))
-		scrolling_text_flask(msg,speed,font)
-		return redirect(url_for('index'),code=302)
+
+@app.route('/text/scrolling/', methods=['POST'])
+def text_scrolling():
+    if hasattr(request, 'form'):
+        msg = request.form['text']
+        speed = (11 - int(request.form['speed'])) * 0.01
+        font = int(request.form['font'])
+        print('speed {} font {}'.format(speed, font))
+        scrolling_text_flask(msg, speed, font)
+        return redirect(url_for('index'), code=302)
+
+
+@app.route('/test/text/scrolling/', methods=['POST'])
+def test_text_scrolling():
+    if hasattr(request, 'form'):
+        msg = request.form['text']
+        speed = int(request.form['speed'])
+        font = int(request.form['font'])
+
+        test_scrolling_text_flask(msg, speed, font)
+
+        return Response(msg)
+
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5123)
-
-
+    app.run(debug=True, host='0.0.0.0', port=5123)
--- matrix.py
+++ matrix.py
@@ -1,4 +1,3 @@
-
 import re
 import time
 import argparse
@@ -8,7 +7,6 @@ from luma.core.render import canvas
 from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
-
 """
 Como conectar
 VCC negro 5V
@@ -19,27 +17,29 @@ clk azul 11 sclk
 """
 
 
-def staticText(device,msg):
-	with canvas(device) as draw:
-		text(draw, (0, 0), msg, fill="white")
-	time.sleep(2)
+def staticText(device, msg):
+    with canvas(device) as draw:
+        text(draw, (0, 0), msg, fill="white")
+    time.sleep(2)
 
-def scrolling_text(device,msg,speed=0.04,font=TINY_FONT):
-	show_message(device, msg, fill="white", font=font,scroll_delay=speed)	
+
+def scrolling_text(device, msg, speed=0.04, font=TINY_FONT):
+    show_message(device, msg, fill="white", font=font, scroll_delay=speed)
+
 
 def main():
-	serial = spi(port=0, device=0, gpio=noop())
-	device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
-	
-	
-def ascii_char(device,pic):
-	with canvas(device) as draw:
-		text(draw, (0, 0), chr(pic), fill="white")
-	time.sleep(2)
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
+
+
+def ascii_char(device, pic):
+    with canvas(device) as draw:
+        text(draw, (0, 0), chr(pic), fill="white")
+    time.sleep(2)
 
 
 # def keyboard_2(device):
-# 	if keyboard.is_pressed('up'):#if key 'q' is pressed 
+# 	if keyboard.is_pressed('up'):#if key 'q' is pressed
 # 		ascii_char(device,24)
 # 		pass
 # 	elif keyboard.is_pressed('down'):
@@ -53,37 +53,49 @@ def ascii_char(device,pic):
 # 		pass
 # 	else:
 # 		pass
-		
-def scrolling_text_flask(msg,speed,font_index=1):
-	"""
-	This is the code that gets called by the flask app
-	msg: String message to scroll(Not UTF-8 compilant)
-	speed: speed delay. The higher, the slower.
-	font_index: it was easier for us to use numbers 
-	instead of the actual font names.
-	"""
-	serial = spi(port=0, device=0, gpio=noop())
-	device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
-	fonts=[CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
-	scrolling_text(device,msg,speed,fonts[font_index])
-			
+
+
+def test_scrolling_text_flask(msg, speed, font_index=1):
+    print('Processing Msg: {} Speed: {} Font: {}\n'.format(
+        msg, 
+        speed, 
+        font_index,
+    ))
+    # Will process 1-10 characters per second depending on speed
+    time.sleep(len(msg) / speed)
+
+
+
+def scrolling_text_flask(msg, speed, font_index=1):
+    """
+    This is the code that gets called by the flask app
+    msg: String message to scroll(Not UTF-8 compilant)
+    speed: speed delay. The higher, the slower.
+    font_index: it was easier for us to use numbers 
+    instead of the actual font names.
+    """
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
+    fonts = [CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
+    scrolling_text(device, msg, speed, fonts[font_index])
+
 
 if __name__ == "__main__":
-	"""
-	Testing code!
-	You shouldn't worry about it unless you want to test your led matrix beforehand.
-	"""
-	serial = spi(port=0, device=0, gpio=noop())
-	device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
-	fonts=[CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
-	while True:
-		#fi=input("Fuente: 0 CP437_FONT, 1 TINY_FONT,  2 SINCLAIR_FONT, 3 LCD_FONT")
-		#keyboard_2(device)
-		#speed = input("velocidad")
-		#mensaje = str(input("mensaje"))
-		#scrolling_text(device,mensaje,speed,fonts[fi])
-		#scrolling_text(device,mensaje)
-		x=input()
-		ascii_char(device,x)
-		#with canvas(device) as draw:
-		#	text(draw, (0, 0), "R", fill="white")
+    """
+    Testing code!
+    You shouldn't worry about it unless you want to test your led matrix beforehand.
+    """
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
+    fonts = [CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
+    while True:
+        #fi=input("Fuente: 0 CP437_FONT, 1 TINY_FONT,  2 SINCLAIR_FONT, 3 LCD_FONT")
+        #keyboard_2(device)
+        #speed = input("velocidad")
+        #mensaje = str(input("mensaje"))
+        #scrolling_text(device,mensaje,speed,fonts[fi])
+        #scrolling_text(device,mensaje)
+        x = input()
+        ascii_char(device, x)
+        #with canvas(device) as draw:
+        #	text(draw, (0, 0), "R", fill="white")
